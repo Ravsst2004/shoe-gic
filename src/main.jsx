3672dfc2426846410deca9c8b528f89fea9bff63
Ravsst2004 <ravindraviswanatha@gmail.com>
2024-08-02 00:02:21 +0800

    Dropdown navbar for mobile

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import Home from "./pages/Home";
+import Navbar from "./components/layouts/Navbar";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
+    <Navbar />
     <Home />
   </React.StrictMode>
 );
